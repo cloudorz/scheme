a21bbf6c585506fc34bc59f31781c323dc39fc47
Cloud <cloudcry@gmail.com>
2017-05-17 14:31:41 +0800

    add exceptions

--- src/Eval.hs
+++ src/Eval.hs
@@ -23,6 +23,47 @@ import Data.Monoid
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
+data LispException
+  = NumArgs Integer [LispVal]
+  | LengthOfList T.Text Int
+  | ExpectedList T.Text
+  | TypeMismatch T.Text LispVal
+  | BadSpecialForm T.Text
+  | NotFunction LispVal
+  | UnboundVar T.Text
+  | Default LispVal
+  | PError String -- from show anyway
+  | IOError T.Text
+
+instance Show LispException where
+  show = T.unpack . showError
+
+unwordsList :: [LispVal] -> T.Text
+unwordsList list = T.unwords $  showVal <$> list
+
+showError :: LispException -> T.Text
+showError err =
+  case err of
+    (IOError txt)          -> T.concat ["Error reading file: ", txt]
+    (NumArgs int args)     -> T.concat ["Error Number Arguments, expected ", T.pack $ show int, " recieved args: ", unwordsList args]
+    (LengthOfList txt int) -> T.concat ["Error Length of List in ", txt, " length: ", T.pack $ show int]
+    (ExpectedList txt)     -> T.concat ["Error Expected List in funciton ", txt]
+    (TypeMismatch txt val) -> T.concat ["Error Type Mismatch: ", txt, showVal val]
+    (BadSpecialForm txt)   -> T.concat ["Error Bad Special Form: ", txt]
+    (NotFunction val)      -> T.concat ["Error Not a Function: ", showVal val]
+    (UnboundVar txt)       -> T.concat ["Error Unbound Variable: ", txt]
+    (PError str)           -> T.concat ["Parser Error, expression cannot evaluate: ",T.pack str]
+    (Default val)          -> T.concat ["Error, Danger Will Robinson! Evaluation could not proceed!  ", showVal val]
+
+safeExec :: IO a -> IO (Either String a)
+safeExec m = do
+  result <- Control.Exception.try m
+  case result of
+    Left (eTop :: SomeException) ->
+      case fromException eTop of
+        Just (enclosed :: LispException) -> return $ Left (show enclosed)
+        Nothing                -> return $ Left (show eTop)
+    Right val -> return $ Right val
 
 basicEnv :: Map.Map T.Text LispVal
 basicEnv = Map.fromList $ primEnv
